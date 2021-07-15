7dbcf63f451a052aeb1bce2d84f4c5820add0990
ваше имя <84718361+Miros908@users.noreply.github.com>
2021-07-15 09:20:33 +0300

    исправил замечания

--- script/popup.js
+++ script/popup.js
@@ -3,6 +3,9 @@ let popup = document.querySelector(".modal");
 let buttonclose = document.querySelector(".button-close");
 let buttonclose2 = document.querySelector(".button-close-2");
 let mapzoom = document.querySelector(".map-zoom");
+let forme = document.querySelector(".appointment-form ");
+let email = document.querySelector(".email-form");
+let names = document.querySelector(".name-form");
 
 
 buttonwrite.addEventListener("click", function(evt) {
@@ -22,5 +25,18 @@ buttonclose.addEventListener("click", function() {
 
 buttonclose2.addEventListener("click", function() {
     mapzoom.classList.add("visualy-hidden");
+
+});
+
+
+forme.addEventListener("submit", function(evt) {
+    if (!names.value || !email.value) {
+        evt.preventDefault();
+
+        popup.classList.remove("bounce");
+        popup.classList.add("error");
+
+    }
+
 
 });
